cdb3cc78effbed00a12b137fb1ae5b8b6e2fc858
Fishbite <43952420+Fishbite@users.noreply.github.com>
2022-06-06 16:05:55 +0100

    tested keyup method

--- src/testKeyup.js
+++ src/testKeyup.js
@@ -1,59 +1,61 @@
 console.log("Tested!!!");
+import { keyboard } from "../lib/keyboard.js";
 
 const actx = new AudioContext();
+const oscOn = {};
 
 // var to hold the state of key down
 // TODO: make this into an array to
 // hold the state of all keys pressed
 let keyPlaying = false;
 
-function playNote(frequency, key) {
+const notes = {
+  q: 440,
+  w: 880,
+};
+
+function playNote(key) {
   console.log(keyPlaying);
 
   let osc, vol;
 
   // TODO: need to check the exact key state
-  if (keyPlaying === false) {
-    keyPlaying = true;
-    osc = actx.createOscillator();
-    vol = actx.createGain();
 
-    osc.frequency.value = frequency;
-    osc.type = "sine";
-    vol.gain.value = 0.1;
+  osc = actx.createOscillator();
+  vol = actx.createGain();
 
-    osc.connect(vol).connect(actx.destination);
+  osc.frequency.value = notes[key];
+  osc.type = "sine";
+  vol.gain.value = 0.1;
 
-    osc.start();
-    console.log(keyPlaying);
-  }
+  oscOn[key] = osc;
 
-  //   osc.onended = function () {
-  //     console.log("ended");
-  //   };
-  //   console.log(key, osc);
+  oscOn[key].connect(vol).connect(actx.destination);
+  console.log(oscOn[key]);
 
-  // TODO: add event listener to specific key
-  // is that possible????
-  window.addEventListener("keyup", keyupHandler, false);
+  osc.start();
+}
 
-  function keyupHandler() {
-    osc.stop();
+// let qKey = keyboard("q");
 
-    keyPlaying = false;
+// qKey.press = function () {
+//   playNote(notes.qKey, qKey);
+// };
 
-    console.log(keyPlaying);
+window.addEventListener("keydown", keydownHandler, false);
+window.addEventListener("keyup", keyupHandler, false);
+
+function keydownHandler(event) {
+  const key = event.key;
+  if (notes[key] && !oscOn[key]) {
+    playNote(key);
   }
 }
 
-document.addEventListener("keypress", keypressHandler, false);
-
-function keypressHandler(event) {
-  let key = event.key;
-  //   console.log(key);
-
-  // TODO: Check if specific key is still pressed
-  if (key === "1" && !keyPlaying) {
-    playNote(440, key);
+function keyupHandler(event) {
+  const key = event.key;
+  if (notes[key] && oscOn[key]) {
+    oscOn[key].stop();
+    delete oscOn[key];
   }
 }
