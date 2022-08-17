ac1f0238adc3b615bd76f50b3959084e344989e1
Fishbite <43952420+Fishbite@users.noreply.github.com>
2022-08-17 12:22:31 +0100

    added drop menu for keyboard info

--- upload.bak.php
+++ upload.bak.php
@@ -1,12 +1,5 @@
 <?php
 
-/* Note: need to look at sanitizing uploaded files
-   as this current method could be vulnerable to
-   PATH TRAVERSAL allowing an attacker to access
-   files that are outside of the intended file
-   destination
-*/
-
 /* Get the name of the uploaded file */
 $filename = $_FILES['file']['name'];
 
