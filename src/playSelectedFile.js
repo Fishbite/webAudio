061a44b0d26984d803a0ff268ad15dd44b7df08f
Fishbite <43952420+Fishbite@users.noreply.github.com>
2022-09-08 15:15:24 +0100

    checks to prevent errors when user cancels select

--- src/playSelectedFile.js
+++ src/playSelectedFile.js
@@ -17,8 +17,9 @@
 This file is work in progress. The intention is to
 provide the user with the facility to select an
 audio file from their local hard drive in order to
-play it whilst they are recording additionanl tunes,
-beats etc. Currently, the user has to upload their
+record additionanl tunes, beats etc. on top of it.
+
+Currently, the user has to upload their
 recording to the server, which is far from ideal.
 
 */
@@ -27,13 +28,28 @@ const actx = new AudioContext();
 function setAudioSource(event) {
   console.log("event:", event, "this:", this);
 
+  // get the file from the input event
+  // this referes to the input element from the document
   const file = this.files[0];
+  // create a property on the window object
   const URL = window.URL;
-  const fileURL = URL.createObjectURL(file);
+
+  console.log("file", file);
+
+  // var to hold an object URL for our selected file
+  let fileURL;
+
+  // CHECK: file has a value
+  if (file) {
+    // assign a new object URL to our variable
+    fileURL = URL.createObjectURL(file);
+  }
 
   console.log("fileURL", fileURL);
 
+  // get the audio element from the document
   const audioNode = document.getElementById("audio");
+  // set the audio source to our object URL
   audioNode.src = fileURL;
 
   // ****** Pre-recorded Music Start ****** \\
@@ -43,31 +59,34 @@ function setAudioSource(event) {
   // A stereo panner node
   let panNode = actx.createStereoPanner();
 
-  // Load the sound \\
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", fileURL, true); // true: load asynchronously, create event
+  // CHECK: fileURL has a value then create an XHR request
+  if (fileURL) {
+    // Load the sound \\
+    let xhr = new XMLHttpRequest();
+    // set xhr.open url value to fileURL
+    xhr.open("GET", fileURL, true); // true: load asynchronously, create event
 
-  // load as a binary file with responsType `arraybuffer`
-  xhr.responseType = "arraybuffer";
+    // load as a binary file with responsType `arraybuffer`
+    xhr.responseType = "arraybuffer";
 
-  // load the sound
-  xhr.send();
+    // load the sound
+    xhr.send();
 
-  xhr.addEventListener("load", onLoad, false);
+    xhr.addEventListener("load", onLoad, false);
 
-  function onLoad(event) {
-    actx.decodeAudioData(
-      xhr.response,
-      (buffer) => {
-        soundBuffer = buffer;
-      },
+    function onLoad(event) {
+      actx.decodeAudioData(
+        xhr.response,
+        (buffer) => {
+          soundBuffer = buffer;
+        },
 
-      (error) => {
-        throw new Error(`It no possibla decodie audioie ${error}`);
-      }
-    );
+        (error) => {
+          throw new Error(`It no possibla decodie audioie ${error}`);
+        }
+      );
+    }
   }
-
   // Set the initial `pan` value
   let pan = "left";
 
@@ -88,6 +107,13 @@ function setAudioSource(event) {
     }
   }
   // ****** Pre-recorded Music End ****** \\
+
+  /* ****** REVOKE object URL ****** */
+  // apparently best practice to revoke the object URL
+  // when safe to do so, because a new one will be
+  // created even if one already exists
+  fileURL = URL.revokeObjectURL(file);
+  console.log("object URL revoked. fileURL is now:", fileURL);
 }
 
 const inputNode = document.getElementById("input");
