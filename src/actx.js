222e5082c380f6fe040ae7b5fd31b79acfa5863b
Fishbite <43952420+Fishbite@users.noreply.github.com>
2023-04-05 14:02:35 +0100

    License notice update

--- src/actx.js
+++ src/actx.js
@@ -1,3 +1,22 @@
+/* // ****** LICENSE NOTICE ****** \\
+      COPYRIGHT: 2020 Stuart Peel 
+      This PROGRAM is distributed under the terms of the:
+      AGPL-3.0-or-later
+
+      What does this  mean? Well, basically it free!
+      Do what you want with it:
+
+        Use it
+        Modify it
+        Give it away as a freebee!
+        Sell it
+        Distribute it
+      
+      If you modify it, please make it available to everyone, 
+      it's part of the license T's & C's
+
+      // ****** LICENSE NOTICE ****** \\
+*/
 // Use this file to create an audio context that
 // you need to use with modules that need one
 // i.e. a module that needs a global audio context
--- src/recordingChain.js
+++ src/recordingChain.js
@@ -1,9 +1,23 @@
 console.log("Connected to the moon!");
 
 /* // ****** LICENSE NOTICE ****** \\
-      COPYRIGHT: 2022 Stuart Peel 
+      COPYRIGHT: 2020 Stuart Peel 
       This PROGRAM is distributed under the terms of the:
       AGPL-3.0-or-later
+
+      What does this  mean? Well, basically it free!
+      Do what you want with it:
+
+        Use it
+        Modify it
+        Give it away as a freebee!
+        Sell it
+        Distribute it
+      
+      If you modify it, please make it available to everyone, 
+      it's part of the license T's & C's
+
+      // ****** LICENSE NOTICE ****** \\
 */
 
 /*
